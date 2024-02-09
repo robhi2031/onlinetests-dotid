6cafdfca176247a33add605bcc77dcb1ab5db88d
Putu Robhi Sanjaya <robhi.sanjaya@gmail.com>
2024-02-09 14:24:07 +0800

    Update RajaongkirController.php

--- app/Http/Controllers/RajaongkirController.php
+++ app/Http/Controllers/RajaongkirController.php
@@ -8,18 +8,34 @@ use App\Services\RajaongkirService;
 class RajaongkirController extends Controller
 {
     protected $rajaongkirService;
-
+    
+    /**
+     * __construct
+     *
+     * @param  mixed $rajaongkirService
+     * @return void
+     */
     public function __construct(RajaongkirService $rajaongkirService)
     {
         $this->rajaongkirService = $rajaongkirService;
     }
-
+    
+    /**
+     * getProvinces
+     *
+     * @return void
+     */
     public function getProvinces()
     {
         $provinces = $this->rajaongkirService->getProvinces();
         return response()->json($provinces);
     }
-
+    
+    /**
+     * getCities
+     *
+     * @return void
+     */
     public function getCities()
     {
         $cities = $this->rajaongkirService->getCities();
